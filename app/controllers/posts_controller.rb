d13afb4c04559f01114a5bb54f8739210ef5fb99
Sienna <remicknsisi@gmail.com>
2023-08-31 15:30:04 -0400

    added create and delete actions for posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -17,4 +17,32 @@ class PostsController < ApplicationController
         end
     end
 
+    def create
+        if @user
+            post = @user.posts.create(post_params)
+            if post.valid?
+                render json: post, status: :created
+            else
+                render json: {errors: post.errors.full_messages}, status: :unprocessable_entity
+            end
+        else
+            render json: {error: "You must login or signup to create a post."}, status: :unauthorized
+        end
+    end
+
+    def delete
+        post = Post.find_by(id: params[:id])
+        if @user && @user.id == post.user_id
+            post.destroy
+            render json: post, status: :ok
+        else
+            render json: {error: "You can only delete your own posts."}, status: :unauthorized
+        end
+    end
+
+    private
+
+    def post_params
+        params.permit(:location, :image, :caption)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,14 @@
 Rails.application.routes.draw do
 
   resources :users
+  resources :posts
 
   get "/users/:id/posts/:post_id", to: "posts#show"
   get "/users/:id/posts/:post_id/likes", to: "users#users_who_liked"
+  post "/signup", to: "users#create"
+  get "/me", to: "users#show"
+  post "/login", to: "sessions#create"
+  delete "/logout", to: "sessions#destroy"
 
   get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }
 end
-
-
-# post "/signup", to: "users#create"
-# get "/me", to: "users#show"
-# post "/login", to: "sessions#create"
-# delete "/logout", to: "sessions#destroy"
-# post "/travel_agents/:id/reviews", to: "reviews#create"
