184097f0495bb6ac33e64b226395107fd95874a0
Sienna <remicknsisi@gmail.com>
2023-08-31 14:30:31 -0400

    removed likes from posts table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_08_31_182859) do
+ActiveRecord::Schema.define(version: 2023_08_31_183005) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -35,7 +35,6 @@ ActiveRecord::Schema.define(version: 2023_08_31_182859) do
     t.string "location"
     t.string "image"
     t.string "caption"
-    t.integer "likes"
     t.integer "user_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
