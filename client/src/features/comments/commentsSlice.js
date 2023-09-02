9cfb597ba1704c02896f49898c3171f4ee78f436
Sienna <remicknsisi@gmail.com>
2023-09-02 15:48:53 -0400

    added comment reducer and action creators

---
+++ client/src/features/comments/commentsSlice.js
@@ -0,0 +1,60 @@
+// Action Creators
+export const addComment = (comment) => {
+    return {
+      type: "comments/add",
+      payload: comment,
+    };
+  };
+  
+  export const removeComment = (id) => {
+    return {
+      type: "comments/remove",
+      payload: id,
+    };
+  };
+  
+//   Reducers
+  const initialState = {
+    entities: [],
+    status: "idle"
+  };
+  
+  export default function commentsReducer(state = initialState, action) {
+    switch (action.type) {
+      case "comments/add":
+        return [...state, action.payload];
+  
+      case "comments/remove":
+        return state.filter((comment) => comment.id !== comment.payload);
+  
+      case "comments/commentsLoading":
+        return {
+          ...state,
+          status: "loading",
+        };
+      
+      case "comments/commentsLoaded":
+        return {
+          ...state,
+          status: "idle",
+          entities: action.payload,
+        };
+  
+      default:
+        return state;
+    }
+  }
+  
+  export function fetchComments() {
+    return function (dispatch) {
+      dispatch({ type: "comments/commentsLoading" });
+      fetch("/comments")
+        .then((response) => response.json())
+        .then((comments) =>
+          dispatch({
+            type: "comments/commentsLoaded",
+            payload: comments,
+          })
+        );
+    };
+  }
--- client/src/reducers.js
+++ client/src/reducers.js
@@ -2,10 +2,12 @@ import { combineReducers } from "redux";
 import usersReducer from "./features/users/usersSlice";
 import likesReducer from "./features/likes/likesSlice";
 import postsReducer from "./features/posts/postsSlice";
+import commentsReducer from "./features/comments/commentsSlice";
 
 const rootReducer = combineReducers({
   users: usersReducer,
-  posts: postsReducer
+  posts: postsReducer,
+  comments: commentsReducer
 //   likes: likesReducer,
 });
 
