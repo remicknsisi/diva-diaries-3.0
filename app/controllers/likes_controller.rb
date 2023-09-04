15de90caf3eca65b78623b82fed1b897227a7788
Sienna <remicknsisi@gmail.com>
2023-09-04 13:42:28 -0400

    added fetch and controller action for liking a post

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -7,4 +7,23 @@ class LikesController < ApplicationController
             render json: {error: "Could not fetch likes."}, status: :not_found
         end
     end
+
+    def create
+        if @user
+            like = @user.likes.create(like_params)
+            if like.valid?
+                render json: like, status: :created
+            else
+                render json: {errors: like.errors.full_messages}, status: :unprocessable_entity
+            end
+        else
+            render json: {error: "You must login or signup to like a post."}, status: :unauthorized
+        end
+    end
+
+    private
+
+    def like_params
+        params.permit(:post_id)
+    end
 end
--- client/src/features/likes/DisplayLikes.js
+++ client/src/features/likes/DisplayLikes.js
@@ -1,11 +1,14 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { fetchLikes } from './likesSlice';
 import EmptyHeartIcon from "../EmptyHeartIcon";
 import { addLike } from "./likesSlice"
+import { useParams, useNavigate } from "react-router-dom";
 
 function DisplayLikes({ id }) {
+    const [errorsList, setErrorsList] = useState([])
     const dispatch = useDispatch();
+    const navigate = useNavigate()
 
     const likes = useSelector((state) => state.likes.entities);
 
@@ -14,7 +17,25 @@ function DisplayLikes({ id }) {
     }, [])
 
     function handleOnClick() {
-        dispatch(addLike());
+        fetch(`/likes`, {
+            method: 'POST',
+            headers: {"Content-Type": "application/json"},
+            body: JSON.stringify({
+                post_id: id
+        })
+          })
+          .then(res => {
+            if(res.ok){
+                res.json().then((newLike) => {
+                    dispatch(addLike(newLike));
+                    navigate('/')})
+            } else {
+                res.json().then((message) => {
+                    const errorLis = message.errors.map(error => <li key={error}>{error}</li>)
+                    setErrorsList(errorLis)
+                })
+            }
+        })
     }
 
     const likesToDisplay = [...likes].filter((l) => l.post_id === id)
@@ -22,6 +43,7 @@ function DisplayLikes({ id }) {
   return (
     <div>
         <button onClick={handleOnClick}><EmptyHeartIcon/> {likesToDisplay.length} Likes</button>
+        <p className="error-message">{errorsList}</p>
     </div>
   );
 }
