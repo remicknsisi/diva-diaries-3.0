e1bd78d63a762b5f2252a8b465139d37584d7f96
Sienna <remicknsisi@gmail.com>
2023-09-10 17:02:58 -0400

    built backend index and create actions for direct messages

--- app/controllers/direct_messages_controller.rb
+++ app/controllers/direct_messages_controller.rb
@@ -1,3 +1,25 @@
 class DirectMessagesController < ApplicationController
-    
+    def index
+        if @user
+            dms = @user.direct_messages 
+            render json: dms, status: :ok
+        else
+            render json: {error: "Could not fetch DMs."}, status: :not_found
+        end
+    end
+
+    def create
+        if @user
+            dm = @user.direct_messages.create(dm_params)
+            render json: dm, status: :created
+        else
+            render json: {error: "You must login or signup to send a DM."}, status: :unauthorized
+        end
+    end
+
+    private
+
+    def dm_params
+        params.permit(:recipient_id, :content)
+    end
 end
--- app/serializers/direct_message_serializer.rb
+++ app/serializers/direct_message_serializer.rb
@@ -1,6 +1,6 @@
 class DirectMessageSerializer < ActiveModel::Serializer
     attributes :id, :sender_id, :recipient_id, :content
   
-    # belongs_to :user
+    belongs_to :user
     # belongs_to :post
 end
